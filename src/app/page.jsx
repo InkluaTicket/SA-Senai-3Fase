61d02848716ae845d648075ec3656dfc3a323b5c
SAMUEL BORDIGNON WIEGAND <samuel_wiegand@estudante.sesisenai.org.br>
2025-03-19 17:24:34 -0300

    colocando o next

---
+++ src/app/page.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1 className=" font-bold underline text-blue-500">
+        Hello world!
+        
+      </h1>
+      <div className="flex w-full bg-gray-600 justify-between">
+        <p className='text-red-900'>
+          olaaaaaa111
+        </p>
+        <p className='text-red-900'>
+          olaaaaaa
+        </p>
+        <p className='text-red-900'>
+          olaaaaaa
+        </p>
+        <p className='text-red-900'>
+          olaaaaaa
+        </p>
+        <p className='text-red-900'>
+          olaaaaaa333
+        </p>
+      </div>
+    </div>
+  )
+}
