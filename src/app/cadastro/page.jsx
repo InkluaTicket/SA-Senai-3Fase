a14ad1eedace365580b43fd8d83a5e04411e4250
samuel-bordignon <samuelborwie@gmail.com>
2025-03-26 17:22:16 -0300

    começando a tela de cadastro cliente parte 2

--- src/app/cadastro/page.jsx
+++ src/app/cadastro/page.jsx
@@ -3,51 +3,53 @@ import React from 'react'
 function page() {
     return (
         <div className="bg-laranja-principal-500 h-screen flex">
-            <div className="bg-cinza-principal-50">
-                <div className="">
+            <div className="bg-cinza-principal-50 w-2/5 px-40 flex items-center">
+                <div className=" w-full flex flex-col gap-10">
                     <div className="">
-                        <div className="">
-                            <h1>Cadastre-se</h1>
+                        <div className="flex items-center gap-4">
+                            <h1 className='font-semibold text-roxo-pincipal-600 text-5xl font-principal'>Cadastre-se</h1>
                             <img src="./icons/ticket-vazado.svg" alt="" />
                         </div>
-                        <p>Todos merecem viver grandes eventos</p>
+                        <p className='font-principal text[16px]'>Todos merecem viver grandes eventos</p>
                     </div>
                     <div className="">
-                        <div className="">
-                            <label htmlFor="">E-mail</label>
-                            <input type="text" />
+                        <div className="container-input">
+                            <label htmlFor="" className='label-crud'>E-mail</label>
+                            <input type="text" className='input-crud' />
+                        </div>
+
+                        <div className="grid grid-cols-2 gap-4">
+                            <div className="container-input">
+                                <label htmlFor="" className='label-crud'>Nome</label>
+                                <input type="text" className='input-crud' />
+                            </div>
+                            <div className="container-input">
+                                <label htmlFor="" className='label-crud'>Sobrenome</label>
+                                <input type="text" className='input-crud' />
+                            </div>
+                        </div>
+                        <div className="container-input">
+                            <label htmlFor="" className='label-crud'>Senha</label>
+                            <input type="text" className='input-crud' />
+                        </div>
+
+                        <div className="container-input">
+                            <label htmlFor="" className='label-crud'>Confirmar Senha</label>
+                            <input type="text" className='input-crud' />
                         </div>
                     </div>
                     <div className="">
-                        <div className="">
-                            <label htmlFor="">Nome</label>
-                            <input type="text" />
-                        </div>
-                        <div className="">
-                            <label htmlFor="">Sobrenome</label>
-                            <input type="text" />
-                        </div>
-                    </div>
-                    <div className="">
-                        <div className="">
-                            <label htmlFor="">Senha</label>
-                            <input type="text" />
-                        </div>
-                    </div>
-                    <div className="">
-                        <div className="">
-                            <label htmlFor="">Confirmar Senha</label>
-                            <input type="text" />
-                        </div>
-                    </div>
                     <button className='bg-roxo-pincipal-600'>
                         Continuar
                     </button>
                     <p>Já possui uma conta?</p>
+                    </div>
                 </div>
             </div>
-            <div className="">
-                <img src="mulher-vazado.svg" alt="" />
+            <div className="flex flex-col justify-between items-end px-6 py-11">
+                <img src="./icons/logo-icon-roxo.svg" alt="" />
+
+                <img src="mulher-vazado.svg" alt="" className='w-full' />
             </div>
         </div>
     )
