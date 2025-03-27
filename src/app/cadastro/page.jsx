652f4fb2903fc4b90cf3e7e38d6a08fe6a326a5a
samuel-bordignon <samuelborwie@gmail.com>
2025-03-27 16:00:06 -0300

    começando a tela de cadastro cliente parte 3

--- src/app/cadastro/page.jsx
+++ src/app/cadastro/page.jsx
@@ -1,21 +1,19 @@
-import React from 'react'
-
 function page() {
     return (
-        <div className="bg-laranja-principal-500 h-screen flex">
-            <div className="bg-cinza-principal-50 w-2/5 px-40 flex items-center">
-                <div className=" w-full flex flex-col gap-10">
+        <div className="bg-laranja-principal-500 h-screen flex font-principal">
+            <div className="bg-[#F9FAFC] w-6/12 px-35 flex items-center">
+                <div className=" w-full flex flex-col gap-12">
                     <div className="">
-                        <div className="flex items-center gap-4">
-                            <h1 className='font-semibold text-roxo-pincipal-600 text-5xl font-principal'>Cadastre-se</h1>
+                        <div className="flex items-center">
+                            <h1 className='font-semibold text-roxo-principal-600 text-6xl flex-nowrap font-principal'>Cadastre-se</h1>
                             <img src="./icons/ticket-vazado.svg" alt="" />
                         </div>
-                        <p className='font-principal text[16px]'>Todos merecem viver grandes eventos</p>
+                        <p className='font-principal font-medium text[16px]'>Todos merecem viver grandes eventos</p>
                     </div>
-                    <div className="">
+                    <div className="flex flex-col gap-3">
                         <div className="container-input">
                             <label htmlFor="" className='label-crud'>E-mail</label>
-                            <input type="text" className='input-crud' />
+                            <input type="text" className='input-crud'  placeholder="seunome@exemplo.com"/>
                         </div>
 
                         <div className="grid grid-cols-2 gap-4">
@@ -30,26 +28,27 @@ function page() {
                         </div>
                         <div className="container-input">
                             <label htmlFor="" className='label-crud'>Senha</label>
-                            <input type="text" className='input-crud' />
+                            <input type="password" className='input-crud' />
                         </div>
 
                         <div className="container-input">
                             <label htmlFor="" className='label-crud'>Confirmar Senha</label>
-                            <input type="text" className='input-crud' />
+                            <input type="password" className='input-crud' />
                         </div>
                     </div>
-                    <div className="">
-                    <button className='bg-roxo-pincipal-600'>
-                        Continuar
-                    </button>
-                    <p>Já possui uma conta?</p>
+                    <div className="flex flex-col justify-center items-center gap-3">
+                        <button className='btn-crud'>
+                            Continuar
+                        </button>
+                        <p className="text-roxo-principal-600 font-semibold text-sm">Já possui uma conta?</p>
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col justify-between items-end px-6 py-11">
-                <img src="./icons/logo-icon-roxo.svg" alt="" />
-
-                <img src="mulher-vazado.svg" alt="" className='w-full' />
+            <div className="flex flex-col justify-start items-center px-6 py-11 gap-16">
+                <div className="w-full flex justify-end">
+                    <img src="./icons/logo-icon-roxo.svg" alt="" className='items-end' />
+                </div>
+                    <img src="mulher-vazado.svg" alt="" className='w-12/16' />
             </div>
         </div>
     )
