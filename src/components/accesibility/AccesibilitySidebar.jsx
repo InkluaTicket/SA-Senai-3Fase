fa50f8d05e544abb8e0c7153464768fcf5144072
samue-bordignon <samuelborwie@gmail.com>
2025-03-31 06:49:50 -0300

    muuuuuooonte de coisas

--- src/components/accesibility/AccesibilitySidebar.jsx
+++ src/components/accesibility/AccesibilitySidebar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import OutButton from './OutButton'
 import Option from './Option'
 
 export default function AccesibilitySidebar() {
+
+    const { toggleSetting } = useContext(GlobalContext)
+
     return (
         <div className='w-6/18 h-screen bg-white absolute top-0 left-0 flex flex-col'>
             <div className="bg-roxo-principal-600 w-full flex justify-between items-center p-4 pl-6 ">
@@ -15,33 +18,33 @@ export default function AccesibilitySidebar() {
                 <div className="flex flex-col gap-6">
                     <p className='text-azul-principal-950 font-medium text-[18px]'>Leitura e Texto</p>
                     <div className="grid grid-cols-2 gap-3">
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
-                        <Option icon={'AlturaTexto.svg'} title={'Altura da Linha'}/>
-                        <Option icon={'EspacamentoLetra.svg'} title={'Espaçamento entre Letras'}/>
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'AlturaLetra.svg'} title={'Altura da Linha'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'EspacamentoLetra.svg'} title={'Espaçamento entre Letras'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'FonteDislexa.svg'} title={'Fonte para Dislexa'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'CabecalhosDestaque.svg'} title={'Cabeçalhos Destacados'}/>
                     </div>
                     <hr className='text-[#CDD1DB] border-1' />
                 </div>
                 <div className="flex flex-col gap-6">
                     <p className='text-azul-principal-950 font-medium text-[18px]'>Leitura e Texto</p>
                     <div className="grid grid-cols-2 gap-3">
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
                     </div>
                     <hr className='text-[#CDD1DB] border-1' />
                 </div>
                 <div className="flex flex-col gap-6">
                     <p className='text-azul-principal-950 font-medium text-[18px]'>Leitura e Texto</p>
                     <div className="grid grid-cols-2 gap-3">
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
-                        <Option icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
+                        <Option onClick={toggleSetting} value={'monochrome'} icon={'TamanhoTexto.svg'} title={'Tamanho do Texto'}/>
                     </div>
                     <hr className='text-[#CDD1DB] border-1' />
                 </div>
--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -4,18 +4,41 @@
 import { createContext, useState } from "react"
 
 
-export const GlobalContext = createContext();
+export const GlobalContext = createContext()
 
 export const GlobalContextProvider = ({ children }) => {
 
-  const [ nome , setNome ] = useState("samuel");
+  const [settings, setSettings] = useState({
+    textSize: false,
+    lineHeight: false,
+    letterSpacing: false,
+    dyslexiaFont: false,
+    highlightHeadings: false,
+    invertColors: false,
+    highSaturation: false,
+    lowSaturation: false,
+    highContrastDark: false,
+    highContrastLight: false,
+    monochrome: false,
+    daltonismFilters: false,
+    largeCursor: false,
+    highlightLinks: false,
+    focusReading: false,
+    readingGuide: false,
+  })
 
+  const toggleSetting = (key) => {
+    setSettings((prev) => ({
+      ...prev,
+      [key]: !prev[key],
+    }))
+  }
+  
 
 
   return(
     <GlobalContext.Provider value={{
-      nome,
-      setNome
+      toggleSetting
     }}>
       {children}
     </GlobalContext.Provider>
